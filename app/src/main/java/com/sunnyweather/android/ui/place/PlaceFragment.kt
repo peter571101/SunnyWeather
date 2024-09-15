632ba29319a8f10466f95808e2e06161f1403271
Tony <gld131571314@gmail.com>
2024-09-15 18:31:29 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -1,6 +1,7 @@
 package com.sunnyweather.android.ui.place
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sunnyweather.android.R
+import com.sunnyweather.android.ui.weather.WeatherActivity
 
 class PlaceFragment: Fragment() {
     val viewModel by lazy{ ViewModelProvider(this).get(PlaceViewModel::class.java)}
@@ -29,6 +31,17 @@ class PlaceFragment: Fragment() {
     @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        if(viewModel.isPlaceSaved()){
+            val place = viewModel.getSavedPlace()
+            val intent = Intent(context,WeatherActivity::class.java).apply {
+                putExtra("location_lng",place.location.lng)
+                putExtra("location_lat",place.location.lat)
+                putExtra("place_name",place.name)
+            }
+            startActivity(intent)
+            activity?.finish()
+            return
+        }
         bgImageView = view.findViewById(R.id.bgImageView)
         recyclerView = view.findViewById(R.id.recyclerView)
         searchPlaceEdit = view.findViewById(R.id.searchPlaceEdit)
---
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -0,0 +1,113 @@
+package com.sunnyweather.android.ui.weather
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowInsets
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.sunnyweather.android.R
+import com.sunnyweather.android.databinding.ActivityWeatherBinding
+import com.sunnyweather.android.logic.model.Weather
+import com.sunnyweather.android.logic.model.getSky
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class WeatherActivity : AppCompatActivity() {
+    val viewModel by lazy{ ViewModelProvider(this).get(WeatherViewModel::class.java)}
+    private lateinit var placeName  : TextView
+    private lateinit var currentTemp : TextView
+    private lateinit var currentSky : TextView
+    private lateinit var currentAQI : TextView
+    private lateinit var nowLayout: View
+    private lateinit var forecastLayout: LinearLayout
+    private lateinit var coldRiskText: TextView
+    private lateinit var dressingText: TextView
+    private lateinit var ultravioletText: TextView
+    private lateinit var carWashingText: TextView
+    private lateinit var weatherLayout: ScrollView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+        val binding = ActivityWeatherBinding.inflate(layoutInflater)
+        placeName = findViewById(R.id.placeName)
+        currentTemp = findViewById(R.id.currentTemp)
+        currentSky = findViewById(R.id.currentSky)
+        currentAQI = findViewById(R.id.currentAQI)
+        nowLayout = findViewById(R.id.nowLayout)
+        forecastLayout = findViewById(R.id.forecastLayout)
+        coldRiskText = findViewById(R.id.coldRiskText)
+        dressingText = findViewById(R.id.dressingText)
+        ultravioletText = findViewById(R.id.ultravioletText)
+        carWashingText = findViewById(R.id.carWashingText)
+        weatherLayout = findViewById(R.id.weatherLayout)
+        if(viewModel.locationLng.isEmpty()){
+            viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
+        }
+        if(viewModel.locationLat.isEmpty()){
+            viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
+        }
+        if(viewModel.placeName.isEmpty()){
+            viewModel.placeName = intent.getStringExtra("place_name") ?: ""
+        }
+        viewModel.weatherLiveData.observe(this, Observer { result->
+            val weather = result.getOrNull()
+            if(weather != null){
+                showWeatherInfo(weather)
+            }else{
+                Toast.makeText(this,"无法成功获取天气信息",Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+        viewModel.refreshWeather(viewModel.locationLng,viewModel.locationLat)
+    }
+
+    private fun showWeatherInfo(weather: Weather){
+        placeName.text = viewModel.placeName
+        val realtime = weather.realtime
+        val daily = weather.daily
+        val currentTempText = "${realtime.temperature.toInt()}℃"
+        currentTemp.text = currentTempText
+        currentSky.text = getSky(realtime.skycon).info
+        val currentPM25Text = "空气指数${realtime.airQuality.aqi.chn.toInt()}"
+        currentAQI.text = currentPM25Text
+        nowLayout.setBackgroundColor(getSky(realtime.skycon).bg)
+        forecastLayout.removeAllViews()
+        val days = daily.skycon.size
+        for(i in 0 until days){
+            val skycon = daily.skycon[i]
+            val temperature = daily.temperature[i]
+            val view = LayoutInflater.from(this).inflate(R.layout.forecast_item,forecastLayout,false)
+            val dataInfo = view.findViewById(R.id.dataInfo) as TextView
+            val skyIcon = view.findViewById(R.id.skyIcon) as ImageView
+            val skyInfo = view.findViewById(R.id.skyInfo) as TextView
+            val temperatureInfo = view.findViewById(R.id.temperatureInfo) as TextView
+            val simpleDataFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            dataInfo.text = simpleDataFormat.format(skycon.date)
+            val sky = getSky(skycon.value)
+            skyIcon.setImageResource(sky.icon)
+            skyInfo.text = sky.info
+            val tempText = "${temperature.min.toInt()} ~ ${temperature.max.toInt()}℃"
+            temperatureInfo.text = tempText
+            forecastLayout.addView(view)
+        }
+        val lifeIndex = daily.lifeIndex
+        coldRiskText.text = lifeIndex.coldRisk[0].desc
+        dressingText.text = lifeIndex.dressing[0].desc
+        ultravioletText.text = lifeIndex.ultraviolet[0].desc
+        carWashingText.text = lifeIndex.carWashing[0].desc
+        weatherLayout.visibility = View.VISIBLE
+    }
+}
